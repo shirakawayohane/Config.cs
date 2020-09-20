df5c451bf462e7d0fd75353f1d1cfc1f709ecb9a
WiZ3910 <mikuto.matsuo@icloud.com>
2020-09-21 04:24:18 +0900

    プロジェクト ファイルを追加します。

---
+++ StaticSerializer.Console/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using TypedConfig.Serialization;
+using System.Reflection;
+namespace TypedConfig.CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var serializer = new Serializer(typeof(TestClass),
+                new SerializeOption()
+                {
+                    EnumAsString = false,
+                    BindingFlags = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static
+                });
+            var serialized = serializer.Serialize();
+            Console.WriteLine(System.Text.Encoding.UTF8.GetString(serialized));
+            TestClass.value1 = 3;
+            TestClass.value2 = "def";
+            TestClass.alphabet = Alphabet.C;
+            TestClass.ChildClass.value = 1.5f;
+            TestClass.ChildClass.GrancChildClass.message = "みよーん";
+            serialized = serializer.Serialize();
+            Console.WriteLine(System.Text.Encoding.UTF8.GetString(serialized));
+        }
+    }
+    public static class TestClass
+    {
+        public static int value1 = 1;
+        public static string value2 = "abc";
+        public static Alphabet alphabet = Alphabet.B;
+
+        public static class ChildClass
+        {
+            public static float value = 1;
+            public static class GrancChildClass
+            {
+                public static string message = "hello";
+            }
+        }
+    }
+    public enum Alphabet
+    {
+        A,B,C
+    }
+}
---
+++ StaticSerializer/SerializeOption.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Reflection;
+
+namespace TypedConfig.Serialization
+{
+    public class SerializeOption
+    {
+        public bool EnumAsString = false;
+        public BindingFlags BindingFlags = BindingFlags.Public | BindingFlags.Static;
+    }
+}
