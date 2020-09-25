fbf690fa786563f83d28390f8202d74170c83f4a
WiZ3910 <mikuto.matsuo@icloud.com>
2020-09-25 16:37:02 +0900

    Add UnityClient

---
+++ Config.Cs.UnityClient/Assets/Scripts/Config.cs/StaticSerializer/SerializeOption.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Reflection;
+
+namespace Config.cs
+{
+    public class SerializeOption
+    {
+        public bool EnumAsString = false;
+        public BindingFlags BindingFlags = BindingFlags.Public | BindingFlags.Static;
+    }
+}
