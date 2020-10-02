b9b8ee538d82e4797155995146650ff955dfa213
WiZ3910 <mikuto.matsuo@icloud.com>
2020-10-03 01:01:20 +0900

    Change pretty print option default to true

--- Unity/Config.cs/StaticSerializer/SerializeOption.cs
+++ Unity/Config.cs/StaticSerializer/SerializeOption.cs
@@ -2,14 +2,12 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Reflection;
-using Utf8Json;
 
 namespace Config.cs
 {
     public class SerializeOption
     {
         public bool EnumAsString = false;
-        public bool PrettyPrint = true;
         public BindingFlags BindingFlags = BindingFlags.Public | BindingFlags.Static;
     }
 }
